cb8ab7ec3fbc5fc7d2ef92b366187a01536fc75b
dnbi <51847115+dnbi@users.noreply.github.com>
2020-11-26 22:05:45 +0900

    Update Senior_MenuViewer
    메뉴이름에서 차가운, 따뜻한 글자색 추가

--- app/src/main/java/com/example/kioskmainpage/Utilities/Senior_MenuViewer.java
+++ app/src/main/java/com/example/kioskmainpage/Utilities/Senior_MenuViewer.java
@@ -1,9 +1,13 @@
 package com.example.kioskmainpage.Utilities;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -36,6 +40,15 @@ public class Senior_MenuViewer extends LinearLayout {
 
     public void setItem(Senior_MenuItem senior_menuitem){
         menu_name_view.setText(senior_menuitem.getName());
+        String menu_name = senior_menuitem.getName();
+        int tempIndex = -1;
+        if ((tempIndex = menu_name.indexOf("차가운")) > -1){
+            Spannable span = (Spannable) menu_name_view.getText();
+            span.setSpan(new ForegroundColorSpan(Color.BLUE), tempIndex, tempIndex+3, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        } else if ((tempIndex = menu_name.indexOf("따뜻한")) > -1){
+            Spannable span = (Spannable) menu_name_view.getText();
+            span.setSpan(new ForegroundColorSpan(Color.RED), tempIndex, tempIndex+3, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        }
         menu_price_view.setText(senior_menuitem.getPrice()+"원");
         menu_image_view.setImageResource(senior_menuitem.getImage());
     }
